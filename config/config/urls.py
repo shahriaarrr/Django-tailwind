7b68ecd5ede8639292c9420c14c91989eaa29f1a
shahriaarrr <shahriaarrr@gmail.com>
2023-04-12 05:53:26 +0430

    config static files

--- config/config/urls.py
+++ config/config/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
