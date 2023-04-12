e1be7cc090daa08986a46f11888b9847a8c1ff2f
shahriaarrr <shahriaarrr@gmail.com>
2023-04-12 06:12:48 +0430

    build home page

--- config/config/urls.py
+++ config/config/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 
+from .views import *
+
 urlpatterns = [
+    path('', HomeView.as_view(), name="home"),
     path('admin/', admin.site.urls),
 ]
 
