8e739e9ece79d403fe296fee7e21839268abe6d2
shahriaarrr <shahriaarrr@gmail.com>
2023-04-12 06:59:24 +0430

    add some style to index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./Config/**/*.{html, js}"],
   theme: {
+    colors: {
+      black: colors.stone,
+      sky: colors.sky,
+      violet: colors.violet
+    },
     extend: {},
   },
   plugins: [],
